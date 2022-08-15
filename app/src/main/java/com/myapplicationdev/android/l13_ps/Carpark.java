e70c64e5240785630605bcc05af5ba748cb2c911
Starguardian09 <21021646@rp.edu.sg>
2022-08-15 21:51:51 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/l13_ps/Carpark.java
@@ -0,0 +1,55 @@
+package com.myapplicationdev.android.l13_ps;
+
+public class Carpark {
+    private int lots;
+    private String type;
+    private int available;
+    private String carparknum;
+
+    public String getCarparknum() {
+        return carparknum;
+    }
+
+    public void setCarparknum(String carparknum) {
+        this.carparknum = carparknum;
+    }
+
+    public String toString() {
+        return "Carpark" +
+                "\ncarpark number: " + carparknum +
+                "\nlots: " + lots +
+                "\ntype: " + type +
+                "\navailable: " + available;
+    }
+
+    public int getLots() {
+        return lots;
+    }
+
+    public void setLots(int lots) {
+        this.lots = lots;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public Carpark(int lots, String type, int available, String carparknum) {
+        this.lots = lots;
+        this.type = type;
+        this.available = available;
+        this.carparknum = carparknum;
+    }
+}
